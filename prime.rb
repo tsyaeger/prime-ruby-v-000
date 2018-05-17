524b44aef4a0cc259bce8cddaba0064ca956ae9a
taryn yaeger <tsyaeger@gmail.com>
2018-05-17 02:08:42 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,12 +2,15 @@
 
 def prime?(num)
   
-  range = (0..num/2 +1)
+  range = (2..num/2 +1)
   
   divs = range.collect{|x| num % x == 0}
+
   
-  return true if divs.length == 0
+  return true if !divs.include?(true)
   
 end
   
+ prime?(17)
+ # prime?(18)
   
