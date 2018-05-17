3070fcc3a8918b77a85f7c76eae59e4b1277619a
taryn yaeger <tsyaeger@gmail.com>
2018-05-17 02:19:18 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,15 +3,16 @@
 
 def prime?(num)
   
-  if num <= 1 
+  if num <= 1
     return false
-
+  elsif num == 2 || num == 4
+    return true
   else
   
     range = (2..num/2 +1)
     divs = range.collect{|x| num % x == 0}
 
-    if divs
+    if !divs.include?(true) ? true : false
 
   end
 end
